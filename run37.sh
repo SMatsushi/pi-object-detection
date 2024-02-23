0c217b101aa2e2572e872bfa8f92458d2831035d
Satoshi Matsushita <satoshi.matsushita@neuma.tech>
2024-02-22 20:48:29 -0800

    Seems initial:   run2.sh, run3.sh, run37.sh, runp.sh

---
+++ run37.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# See: https://www.pyimagesearch.com/2017/09/18/real-time-object-detection-with-deep-learning-and-opencv/
+
+## imutils installed
+python3 real_time_object_detection2.py \
+	--prototxt MobileNetSSD_deploy.prototxt.txt \
+	--model MobileNetSSD_deploy.caffemodel
+
+# python videostream_demo.py --picamera 0
+
+## seems to work with static jpeg
+# python deep_learning_object_detection.py \
+# 	--prototxt MobileNetSSD_deploy.prototxt.txt \
+# 	--model MobileNetSSD_deploy.caffemodel --image images/example_01.jpg 
